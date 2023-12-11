e3836b6789be0eebd4b4f11f0b80b937f580e301
dev-afhasani <dev.afhasani@gmail.com>
2023-12-11 14:39:11 +0700

    Create auth config for login and logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logOut, reset } from '../features/authSlice'
 
 const Navbar = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.auth)
+
+  const logout = () => {
+    dispatch(logOut())
+    dispatch(reset())
+    navigate('/')
+  }
   return (
     <div>
       <nav className="navbar is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
@@ -21,7 +32,7 @@ const Navbar = () => {
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
-                <button className="button is-light">
+                <button onClick={logout} className="button is-light">
                   Log out
                 </button>
               </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { IoPerson, IoPricetag, IoHome, IoLogOut } from 'react-icons/io5'
+import { useDispatch, useSelector } from 'react-redux'
+import { logOut, reset } from '../features/authSlice'
 
 const Sidebar = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.auth)
+
+  const logout = () => {
+    dispatch(logOut())
+    dispatch(reset())
+    navigate('/')
+  }
+
   return (
     <div>
       <aside className="menu pl-2 has-shadow">
@@ -21,7 +33,7 @@ const Sidebar = () => {
           Settings
         </p>
         <ul className="menu-list">
-          <li><button className='button is-white'><IoLogOut /> Logout</button></li>
+          <li><button onClick={logout} className='button is-white'><IoLogOut /> Logout</button></li>
         </ul>
       </aside>
     </div>
