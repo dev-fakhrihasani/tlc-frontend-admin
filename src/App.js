1e825ae3009b8bd14e73d2fb1a71cb317a97a5fe
dev-afhasani <dev.afhasani@gmail.com>
2023-12-11 11:15:24 +0700

    Create product form page for post and update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Users from './pages/Users';
 import Products from './pages/Products';
 import AddUser from './pages/AddUser';
 import EditUser from './pages/EditUser';
+import AddProduct from './pages/AddProduct';
+import EditProduct from './pages/EditProduct';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Route path='/users/add' element={<AddUser />}></Route>
           <Route path='/users/edit/:id' element={<EditUser />}></Route>
           <Route path='/products' element={<Products />}></Route>
+          <Route path='/products/add' element={<AddProduct />}></Route>
+          <Route path='/products/edit/:id' element={<EditProduct />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
