430916a5f49cf71a99ce057d151b04516a9dea3d
dev-afhasani <dev.afhasani@gmail.com>
2023-12-11 10:19:58 +0700

    Create Login and UserList page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
+import Login from './components/Login';
+import Users from './pages/Users';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Login />}></Route>
           <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/users' element={<Users />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Users.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../pages/Layout'
+import UserList from '../components/UserList'
+
+const Users = () => {
+  return (
+    <Layout>
+      <UserList />
+    </Layout>
+  )
+}
+
+export default Users
