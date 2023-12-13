25cc21d0fecc63bcbb561c3bfd3f043f9138c15a
dev-afhasani <dev.afhasani@gmail.com>
2023-12-13 21:36:49 +0700

    Create get API for Volunteers

--- src/components/VolunteerList.jsx
+++ src/components/VolunteerList.jsx
@@ -3,6 +3,17 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const VolunteerList = () => {
+  const [volunteers, setVolunteers] = useState([])
+
+  useEffect(() => {
+    getVolunteers()
+  }, [])
+
+  const getVolunteers = async () => {
+    const response = await axios.get('http://localhost:5000/volunteers')
+    setVolunteers(response.data)
+  }
+
   return (
     <div>
       <h1 className='title'>Volunteers</h1>
@@ -19,16 +30,22 @@ const VolunteerList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <Link className='button is-small is-info'>Edit</Link>
-              <button className='button is-small is-danger'>Delete</button>
-            </td>
-          </tr>
+          {volunteers.map((volunteer, index) => (
+            <tr key={volunteers.id}>
+              <td> {index + 1} </td>
+              <td> {volunteer.name} </td>
+              <td> {volunteer.division}  </td>
+              <td>
+                <figure className='image is-128x128'>
+                  <img src={volunteer.url} alt="admin-img" />
+                </figure>
+              </td>
+              <td>
+                <Link className='button is-small is-info'>Edit</Link>
+                <button className='button is-small is-danger'>Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
