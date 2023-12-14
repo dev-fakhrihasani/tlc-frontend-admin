26ee0ad143e556b74cbccd7427dd803971884269
dev-afhasani <dev.afhasani@gmail.com>
2023-12-14 11:50:10 +0700

    Protecting all pages

--- src/pages/EditTestimony.jsx
+++ src/pages/EditTestimony.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from './Layout'
 import FormEditTestimony from '../components/FormEditTestimony'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { getMe } from '../features/authSlice'
 
 const EditTestimony = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { isError } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    dispatch(getMe())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (isError) {
+      navigate('/')
+    }
+  }, [isError, navigate])
+
   return (
     <div>
       <Layout>
