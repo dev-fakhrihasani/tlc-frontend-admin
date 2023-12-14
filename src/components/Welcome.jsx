6ff49bd31c41179ee835d76e2819343bc2a4398a
dev-afhasani <dev.afhasani@gmail.com>
2023-12-14 23:45:16 +0700

    Update dashboard welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,11 +6,17 @@ const Welcome = () => {
 
   return (
     <div>
-      <h1 className='title'>Dashboard</h1>
-      <h2 className='subtitle'>Welcome Back <strong>{user && user.name}</strong> </h2>
-      <figure className='image is-128x128'>
-        <img src={user && user.url} alt="admin-img" />
-      </figure>
+      <div className="columns">
+        <div className="column is-10-desktop is-10-tablet ">
+          <h1 className='title'>Dashboard</h1>
+          <h2 className='subtitle'>Welcome Back, <strong>{user && user.name}</strong> 😊 </h2>
+        </div>
+        <div className="column">
+          <figure className='image is-128x128'>
+            <img src={user && user.url} alt="admin-img" className='is-rounded' />
+          </figure>
+        </div>
+      </div>
     </div>
   )
 }
